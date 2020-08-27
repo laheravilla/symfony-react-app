72f36267043ba0e4b5142d0963d1c0fd749904a9
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-27 06:55:31 +0200

    property uniqueness

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,15 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
  *
  * Allows GET method. All non listed methods do not have a route
  * Serialization: convert object to json
