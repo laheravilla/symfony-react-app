9e7376f46f53dd2f028f8afbe4552dabf57e345d
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-12 05:24:57 +0200

    only user can view his full profile (email, role)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -119,7 +119,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"post", "put", "get-admin"})
+     * @Groups({"post", "put", "get-admin", "get-owner"})
      * @Assert\NotBlank()
      * @Assert\Email()
      */
@@ -127,7 +127,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="json", length=200)
-     * @Groups({"get-admin"})
+     * @Groups({"get-admin", "get-owner"})
      */
     private $roles;
 
