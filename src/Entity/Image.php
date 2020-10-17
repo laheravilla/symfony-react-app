b726653fa789e14982afafbadfeaa4d8917e5447
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-17 06:17:40 +0200

    implemented image entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class Image
+ * @package App\Entity
+ * @ORM\Entity()
+ * @Vich\Uploadable()
+ * @ApiResource(
+ *
+ * )
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="url")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    private $url;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile($file): self
+    {
+        $this->file = $file;
+        return $this;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+        return $this;
+    }
+}
