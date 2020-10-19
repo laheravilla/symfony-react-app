33fe4ea2cc9628ac6a677ffb5db28aff18b11120
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-19 06:07:15 +0200

    implemented search filter for posts

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
 use App\Repository\BlogPostRepository;
@@ -11,11 +12,21 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
  *
  * * Allows GET method. All non listed methods do not have a route
+ * @ApiFilter(
+ *     SearchFilter::class,
+ *     properties={
+ *          "id": "exact",
+ *          "title": "partial",
+ *          "content": "partial",
+ *          "author": "exact"
+ *     }
+ * )
  * @ApiResource(
  *     itemOperations={
  *          "get"={
