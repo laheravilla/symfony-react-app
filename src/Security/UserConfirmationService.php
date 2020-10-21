6e4c075f0d4a06c04e7ebbdaf46f245a936de15a
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-21 06:26:03 +0200

    installed monologo library fot application login

--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class UserConfirmationService
 {
@@ -15,22 +16,35 @@ class UserConfirmationService
     /** @var EntityManagerInterface */
     private $entityManager;
 
-    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager)
-    {
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger
+    ) {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug("Fetching user by confirmation token");
+
         /** @var User $user */
         $user = $this->userRepository->findOneBy(["confirmationToken" => $confirmationToken]);
 
         // User was NOT found by confirmation token
-        if (!$user) throw new InvalidConfirmationTokenException();
+        if (!$user) {
+            $this->logger->debug("User by confirmation token not found");
+            throw new InvalidConfirmationTokenException();
+        }
 
         $user->setEnabled(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+        $this->logger->debug("Confirmed user by confirmation token");
     }
 }
