ccce07fbd763170306bfc2ef0e62735ad3e060a6
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-23 06:26:52 +0200

    mocked classes, extracted mock factory code

--- tests/EventSubscriber/AuthoredEntitySubscriberTest.php
+++ tests/EventSubscriber/AuthoredEntitySubscriberTest.php
@@ -3,9 +3,15 @@
 namespace App\Tests\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\BlogPost;
+use App\Entity\User;
 use App\EventSubscriber\AuthoredEntitySubscriber;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class AuthoredEntitySubscriberTest extends TestCase
 {
@@ -17,5 +23,52 @@ class AuthoredEntitySubscriberTest extends TestCase
             ['getAuthenticatedUser', EventPriorities::PRE_WRITE],
             $result[KernelEvents::VIEW]
         );
+    }
+
+    public function testSetAuthorCall()
+    {
+        $tokenStorageMock = $this->getTokenStorageMock();
+        $eventMock = $this->getEventMock();
+
+        (new AuthoredEntitySubscriber($tokenStorageMock))->getAuthenticatedUser(
+            $eventMock
+        );
+    }
+
+    /**
+     * @return MockObject|TokenStorageInterface
+     */
+    private function getTokenStorageMock()
+    {
+        $tokenMock = $this->getMockBuilder(TokenInterface::class)
+            ->getMockForAbstractClass();
+        $tokenMock->expects($this->once())
+            ->method("getUser")
+            ->willReturn(new User());
+
+        $tokenStorageMock = $this->getMockBuilder(TokenStorageInterface::class)
+            ->getMockForAbstractClass(); // Get all classes & interfaces
+
+        $tokenStorageMock->expects($this->once())
+            ->method("getToken")
+            ->willReturn($tokenMock);
+
+        return $tokenStorageMock;
+    }
+
+    /**
+     * @return MockObject|ViewEvent
+     */
+    private function getEventMock()
+    {
+        $eventMock = $this->getMockBuilder(ViewEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $eventMock->expects($this->once())
+            ->method("getControllerResult")
+            ->willReturn(new BlogPost());
+
+        return $eventMock;
     }
 }
